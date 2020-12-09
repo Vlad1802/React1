c472425b1da1ba8439535e0bc55947c0c5350355
Vlad1802 <agee-vlad@yandex.ru>
2020-12-09 13:43:55 +0300

    First
    First

---
+++ first/src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends  React.Component {
+    render (){
+        return (
+            <div>
+                <h2>Узнаете погоду ввашем городе!</h2>
+            </div>
+        );
+    }
+}
+
+export default Info;
